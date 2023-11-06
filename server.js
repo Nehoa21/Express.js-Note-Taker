d6cb6e6c5866078b9e9882cca7666a123703067a
Nehoa21 <nehoaakina21@yahoo.com>
2023-11-06 16:02:37 -0700

    added api/notes routes, both get and post

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,5 +8,54 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.use(express.static('public'));
+
+// route to index.html
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// route to notes.html
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+app.get('/api/notes', (req, res) => 
+    fs.readFile('db/db.json', 'utf-8', (err, data) => {
+        const notesJson = JSON.parse(data);
+        res.json(notesJson);
+    })
+);
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+        };
+        readAndAppend(newNote, 'db/db.json');
+        res.json('Note added!')
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+
+
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
